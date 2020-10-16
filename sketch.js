7ffac0c3825a5f5588bcc01496189f8c6aaa819b
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-10-16 09:42:38 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -37,15 +37,17 @@ function setup() {
 	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1, isStatic:true});
 	World.add(world, packageBody);
 	
+       
+	box2 = new Box(400,650,180,20) 
+	box1 = new Box(320,600,20,100)
+	box3 = new Box(480,595,20,90)
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
 	 World.add(world, ground);
 	 
 	
-	box2 = new Box(400,650,180,20) 
-	box1 = new Box(320,600,20,100)
-	box3 = new Box(480,595,20,90)
+	
 
 	Engine.run(engine);
   
